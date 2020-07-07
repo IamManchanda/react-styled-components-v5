f20d10b9fc522d9320407aa54deb82ffec63abcd
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-07 23:23:02 +0530

    Add global styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Fragment } from "react";
+import GlobalStyled from "./global.styled";
 import { ButtonStyled } from "components";
 
 const App = () => (
   <Fragment>
+    <GlobalStyled />
     <h1>App</h1>
     <ButtonStyled>Test</ButtonStyled>
   </Fragment>
---
+++ src/global.styled.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyled = createGlobalStyle`
+  body {
+    background: white;
+    min-height: 100vh;
+    margin: 0;
+    color: black;
+    font-family: "Kaushan Script", cursive;
+  }
+`;
+
+export default GlobalStyled;
