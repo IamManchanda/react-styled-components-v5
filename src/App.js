638b1158d40177ec1ca746be30f7e7570f9208e2
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-07 23:05:17 +0530

    First Styled component!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { ButtonStyled } from "components";
 
-const App = () => <h1>App</h1>;
+const App = () => (
+  <Fragment>
+    <h1>App</h1>
+    <ButtonStyled>Test</ButtonStyled>
+  </Fragment>
+);
 
 export default App;
