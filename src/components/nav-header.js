c52150f31a0d6557744377a4dc4e54125cae6b2f
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-08 13:30:19 +0530

    Styling normal React components + extending existing styled component styles

--- src/components/nav-header.js
+++ src/components/nav-header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link as RRDLink } from "react-router-dom";
 
 const NavHeaderStyled = styled.header`
   height: 60px;
@@ -13,10 +13,39 @@ const NavHeaderStyled = styled.header`
   background: #eee;
 `;
 
+const MenuStyled = styled.nav`
+  display: flex;
+  position: relative;
+  width: initial;
+  border-bottom: none;
+  margin: auto 0 auto auto;
+  font-family: "Open Sans";
+  background: none;
+  left: initial;
+  top: initial;
+`;
+
+const Link = ({ isActive, children, ...otherProps }) => (
+  <RRDLink {...otherProps}>{children}</RRDLink>
+);
+
+const LinkStyled = styled(Link)`
+  padding: 4px 8px;
+  display: block;
+  text-align: center;
+  box-sizing: border-box;
+  margin: auto 0;
+  font-weight: ${({ isActive }) => (isActive ? "bold" : "normal")};
+`;
+
 const NavHeader = () => (
   <NavHeaderStyled>
-    <Link to="/">Home</Link>
-    <Link to="/login">Login</Link>
+    <MenuStyled>
+      <LinkStyled to="/">Home</LinkStyled>
+      <LinkStyled to="/login" isActive>
+        Login
+      </LinkStyled>
+    </MenuStyled>
   </NavHeaderStyled>
 );
 
