27f1bf7eef14cde7d2da95d2277a3d0ca2ce41a1
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-08 18:09:53 +0530

    Implementing the show / hide password toggle!

--- src/components/password-input.js
+++ src/components/password-input.js
@@ -1,6 +1,14 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import styled from "styled-components";
 import { InputStyled } from "components/ui";
+
+const PasswordInputStyledWrapper = styled.div`
+  display: flex;
+
+  ~ div {
+    margin-bottom: 8px;
+  }
+`;
 
 const PasswordInputStyled = styled(InputStyled).attrs(() => ({
   type: "password",
@@ -10,9 +18,38 @@ const PasswordInputStyled = styled(InputStyled).attrs(() => ({
   border-bottom-right-radius: 0;
 `;
 
+const ToggleButtonStyled = styled.div`
+  height: 40px;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+  font-size: 0.9em;
+  display: flex;
+  padding: 8px;
+  border-left: 0;
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px;
+  background-color: white;
+  font-weight: bold;
+  cursor: pointer;
+  user-select: none;
+  color: black;
+`;
+
 const PasswordInput = (props) => {
   const [showPassword, setShowPassword] = useState(false);
-  return <PasswordInputStyled {...props} />;
+  return (
+    <Fragment>
+      <PasswordInputStyledWrapper>
+        <PasswordInputStyled {...props} />
+        <ToggleButtonStyled onClick={() => setShowPassword((state) => !state)}>
+          {showPassword ? "Hide" : "Show"}
+        </ToggleButtonStyled>
+      </PasswordInputStyledWrapper>
+      <div>
+        <p> {showPassword ? props.value : ""}</p>
+      </div>
+    </Fragment>
+  );
 };
 
 export default PasswordInput;
